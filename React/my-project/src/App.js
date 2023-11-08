dad18f53332edb3d965e9b64a743dbfd59da2b80
FarhathH <farhathh0@gmail.com>
2023-11-08 16:16:35 +0000

    Learning about .map function/syntax
    :)

--- React/my-project/src/App.js
+++ React/my-project/src/App.js
@@ -7,29 +7,77 @@ function MyHeading({heading,type}){ //parameter for function
   
   
   //check the condition to effect which heading is displayed.
-  if(heading === "Ⓦⓗⓨ？" && type == 1){
+  //header parameter is a placeholder for new text
+  if(type == 1){
     return( 
       <h1> that's some nice {heading}</h1>
     );
   }
-  else if(heading === "two" && type == 2){
+  else if(type == 2){
     return(
       <h2>Let's get some {heading}</h2>
     );
   }
-  else if(heading === "Penni the dancing Cholo" && type == 3){
+  else if(type == 3){
     return(
       <h3>Wanna see my {heading}?</h3>
     );
   }
-  else if(heading === "=͟͟͞͞ =͟͟͞͞ ﾍ ( ´ Д `)ﾉ" && type == 6){
+  else if(type == 6){
     return(
       <h6>How could this happen to {heading}?</h6>
     );
   }
 }
 
+let siteMapData = {
+  heading:"Tarantulas4U",
+  url:"https://worldoftarantula.com/en-gb"
+}; //Object
 
+let siteMapData2 = [
+  "I will end you",
+  "Get wreaked",
+  "Whacha want Tom? I can't hear ya Tom"
+]; //Array
+
+function ArrayTime({element}){ //function and parameter 
+  
+  //console.log({element})
+  //for(let index = 0; index < element.length; index++){
+   
+      return(
+        element.map((item) =>{ //map function/syntax that prints all indexes in the array
+          console.log(item)
+            return(
+              <p>
+                {item}
+              </p>
+            )
+          
+        })
+      ) 
+  }
+  
+  
+
+
+
+function SiteMapBox({data}){ //function for sitemap
+  
+  console.log(data) //should print in the console
+  return(//using dot specifier to call the objects
+    <>
+    <strong>{data.heading}</strong> 
+    <a  href = {data.url}
+        target = "_blank"> 
+          
+        <h1>˚₊𓆩༺🕷༻𓆪₊˚</h1>
+    </a>
+    </>
+  );
+  
+}
 
 function App() {
   return (
@@ -37,6 +85,14 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
 
+      <ArrayTime
+        element = {siteMapData2}
+      />
+
+        <SiteMapBox //called the function
+          data = {siteMapData}
+          
+        />
         
         <MyHeading //calls function with different response and font type
           heading = {"Ⓦⓗⓨ？"} 
